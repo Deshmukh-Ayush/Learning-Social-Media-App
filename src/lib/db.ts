60dd72f74af6cc061797600ac50eda6c59a3e1a1
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2025-02-21 04:41:50 +0530

    database connection code.

---
+++ src/lib/db.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI in .env.local");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.conn) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw new Error(
+      `Something went wrong while connecting to database. ${error}`
+    );
+  }
+}
+
+export default dbConnect;
