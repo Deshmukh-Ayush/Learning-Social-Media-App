1f4f7a8a02748ed93173bf1d87e6985bfe71df5f
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2025-02-22 20:43:05 +0530

    login schema

--- src/schemas/registerSchema.ts
+++ src/schemas/registerSchema.ts
@@ -1,20 +1,26 @@
-import {z} from "zod"
+import { z } from "zod";
 
 export const usernameValidation = z
-    .string()
-    .min(2, "Username must be atleast 2 characters long")
-    .max(20, "Username must not exceed the limit of 20 characters")
-    .regex(/^[a-zA-z0-9]+$/, "Username must not contain any special characters")
-
+  .string()
+  .min(2, "Username must be atleast 2 characters long")
+  .max(20, "Username must not exceed the limit of 20 characters")
+  .regex(/^[a-zA-z0-9]+$/, "Username must not contain any special characters");
 
 export const emailValidation = z
-    .string()
-    .min(2, "Email must be 2 characters long")
-    .regex(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/, "Not a valid email address")
+  .string()
+  .min(2, "Email must be 2 characters long")
+  .regex(
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    "Not a valid email address"
+  );
 
 export const registerSchema = z.object({
-    username: usernameValidation,
-    email: emailValidation,
-    password: z.string().min(6, {message: "Password must be atleast 5 characters"}),
-    fullName: z.string().min(4, {message: "Full Name must be 4 characters long or higher"})
-})
+  username: usernameValidation,
+  email: emailValidation,
+  password: z
+    .string()
+    .min(6, { message: "Password must be atleast 5 characters" }),
+  fullName: z
+    .string()
+    .min(4, { message: "Full Name must be 4 characters long or higher" }),
+});
