be06d0bd885f8e600de3f98adf0d44c1d0c18148
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2025-03-02 23:46:54 +0530

    imagekit backend basic configuration done

--- src/app/api/imagekit-auth/route.ts
+++ src/app/api/imagekit-auth/route.ts
@@ -2,11 +2,19 @@ import { NextResponse } from "next/server";
 import Imagekit from "imagekit";
 
 const imagekit = new Imagekit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT!,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT!,
 });
 
 export async function GET() {
-    return NextResponse.json(imagekit.getAuthenticationParameters())
+  try {
+    const authenticationParameters = imagekit.getAuthenticationParameters()
+    return NextResponse.json(authenticationParameters);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Imagekit auth failed" },
+      { status: 500 }
+    );
+  }
 }
